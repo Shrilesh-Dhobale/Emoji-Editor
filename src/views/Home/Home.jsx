4f83a9191d975592b3e8ef24ee1da4f2531ea9ce
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-11-20 19:55:19 +0530

    Home.jsx created and bg set

---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <div className="min-h-screen w-full relative">
+        <div
+            className="absolute inset-0 z-0"
+            style={{
+            backgroundImage: `
+            linear-gradient(to right, #e7e5e4 1px, transparent 1px),
+            linear-gradient(to bottom, #e7e5e4 1px, transparent 1px)
+             `,
+            backgroundSize: "20px 20px",
+            backgroundPosition: "0 0, 0 0",
+            maskImage: `
+            repeating-linear-gradient(
+              to right,
+              black 0px,
+              black 3px,
+              transparent 3px,
+              transparent 8px
+            ),
+            repeating-linear-gradient(
+              to bottom,
+              black 0px,
+              black 3px,
+              transparent 3px,
+              transparent 8px
+            ),
+           radial-gradient(ellipse 80% 80% at 100% 100%, #000 50%, transparent 90%)
+            `,
+            WebkitMaskImage: `
+                   repeating-linear-gradient(
+              to right,
+              black 0px,
+              black 3px,
+              transparent 3px,
+              transparent 8px
+            ),
+            repeating-linear-gradient(
+              to bottom,
+              black 0px,
+              black 3px,
+              transparent 3px,
+              transparent 8px
+            ),
+           radial-gradient(ellipse 80% 80% at 100% 100%, #000 50%, transparent 90%)
+           `,
+           maskComposite: "intersect",
+           WebkitMaskComposite: "source-in",
+       }}
+        />
+        </div>
+    </div>
+  )
+}
+
+export default Home
