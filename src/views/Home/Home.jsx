5a91a343a0b8ee0d2360edec80cec88e03a09caf
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-11-21 20:04:47 +0530

    Tailwind removed

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -4,55 +4,8 @@ import './Home.css'
 function Home() {
   return (
     <div>
-      <div className="min-h-screen w-full relative">
-        <div
-            className="absolute inset-0 z-0"
-            style={{
-            backgroundImage: `
-            linear-gradient(to right, #e7e5e4 1px, transparent 1px),
-            linear-gradient(to bottom, #e7e5e4 1px, transparent 1px)
-             `,
-            backgroundSize: "20px 20px",
-            backgroundPosition: "0 0, 0 0",
-            maskImage: `
-            repeating-linear-gradient(
-              to right,
-              black 0px,
-              black 3px,
-              transparent 3px,
-              transparent 8px
-            ),
-            repeating-linear-gradient(
-              to bottom,
-              black 0px,
-              black 3px,
-              transparent 3px,
-              transparent 8px
-            ),
-           radial-gradient(ellipse 80% 80% at 100% 100%, #000 50%, transparent 90%)
-            `,
-            WebkitMaskImage: `
-                   repeating-linear-gradient(
-              to right,
-              black 0px,
-              black 3px,
-              transparent 3px,
-              transparent 8px
-            ),
-            repeating-linear-gradient(
-              to bottom,
-              black 0px,
-              black 3px,
-              transparent 3px,
-              transparent 8px
-            ),
-           radial-gradient(ellipse 80% 80% at 100% 100%, #000 50%, transparent 90%)
-           `,
-           maskComposite: "intersect",
-           WebkitMaskComposite: "source-in",
-       }}
-        />
-        </div>
+      <h1>Welcome to the Emoji Editor</h1>
+      <p>Edit and customize your favorite emojis!</p>
     </div>
   )
 }
