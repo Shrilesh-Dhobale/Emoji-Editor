3faa6e3daf64ccfe59581bfc266f39b949d89e96
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-11-21 20:16:58 +0530

    Home comp imported

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -3,8 +3,8 @@ import './Home.css'
 
 function Home() {
   return (
-    <div>
-      <h1>Welcome to the Emoji Editor</h1>
+    <div className="container">
+      <h1 className="Heading">Welcome to the Emoji Editor</h1>
       <p>Edit and customize your favorite emojis!</p>
     </div>
   )
