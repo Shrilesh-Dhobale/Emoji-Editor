d64c8e3b17fea3a2cafbb15dd84631a5df43db86
dshrileshdhobale@gmail.com <dshrilesh@gmail.com>
2025-11-21 20:42:11 +0530

    Styling added for main container

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -3,11 +3,20 @@ import './Home.css'
 
 function Home() {
   return (
-    <div className="container">
-      <h1 className="Heading">Emoji Editor</h1>
-      <p className='sub-title'>Edit and customize your favorite emojis!</p>
-      <div className="emoji-grid">
-        <h2 className='emoji'>🚗</h2>
+    <div className='main-container'>
+      <div className="container">
+        <h1 className="Heading">Emoji Editor</h1>
+        <p className='sub-title'>Edit and customize your favorite emojis!</p>
+        <div className="emoji-grid">
+          <h2 className='emoji'>🚗</h2>
+        </div>
+      </div>
+      <div className="controls-container">
+        <div className='left-controls'>
+          <div className="font-size">
+            <h2 className='control-heading'>Font Size:</h2>
+          </div>
+        </div>
       </div>
     </div>
   )
